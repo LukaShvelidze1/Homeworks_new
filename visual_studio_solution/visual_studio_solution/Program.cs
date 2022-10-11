438bdce2621772d6d94f3036d5b6eabb46642d57
unknown <lshve16@freeuni.edu.ge>
2022-10-11 13:43:53 +0400

    new homework

---
+++ visual_studio_solution/visual_studio_solution/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        Console.WriteLine("Luka Shvelidze");
+
+        Console.WriteLine(Console.ReadLine());
+    }
+}
