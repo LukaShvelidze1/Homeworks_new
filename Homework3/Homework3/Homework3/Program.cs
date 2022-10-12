2eb6d099ddca66381a5c186ce65572ae1b6fd6b6
unknown <lshve16@freeuni.edu.ge>
2022-10-13 01:17:40 +0400

    not finished

---
+++ Homework3/Homework3/Homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("input number");
+//int number;
+//number = Convert.ToInt32(Console.ReadLine());
+
+//if (number % 5 == 0)
+//{
+//    Console.WriteLine("YES");
+//}
+//else
+//{
+//    Console.WriteLine("NO");
+//}
+
+
