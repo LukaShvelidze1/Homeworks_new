d7c289110b5ddb6209c23bf526a8a3dc0115b7bb
luka shvelidze <lukashvelidze@lukas-MacBook-Pro.local>
2022-10-23 23:50:44 +0400

    unfinished homework6

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Problem #1----
+
+List<int> numbers = new List<int>();
+
+int i;
+int j;
+
+j = Convert.ToInt32(Console.ReadLine());
+
+for (i = 0; i < j; i++)
+{
+    numbers.Add(i);
+    Console.WriteLine(numbers);
+}
---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,88 @@
+﻿//Problem #1----
+
+//topGMethod(0,0,0);
+
+
+
+//static void topGMethod(int a, int b, int c)
+//{
+//    Console.WriteLine("Enter min number: ");
+//    a = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("enter max number: ");
+//    b = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("Enter last number: ");
+//    c = Convert.ToInt32(Console.ReadLine());
+
+//    var numbersBetween = 0;
+
+//    for (int i = 0; i < b; i++)
+//    {
+//        var power = Math.Pow(i, c);
+//        if (power >= a && power <= b)
+//        {
+//            numbersBetween++;
+//        }
+//    }
+//    Console.WriteLine(numbersBetween);
+
+//}
+
+
+//problem #2----
+
+//findTwins();
+
+//static void findTwins()
+//{
+//    string word = Convert.ToString(Console.ReadLine());
+//    char[] wordToArray = word.ToCharArray();
+
+//    var Twin = 0;
+
+//    for (int i = 1; i < wordToArray.Length; i++)
+//    {
+//        for (int j = 0; j < i; j++)
+//        {
+//            if (wordToArray[j] == wordToArray[i])
+//            {
+//                Twin++;
+//            }
+//        }
+
+//    }
+//    Console.WriteLine(Twin);
+//}
+
+//Problem #3----
+
+
+
+static void sameEnding()
+{
+    Console.WriteLine("Input first word: ");
+    string firstWord = Convert.ToString(Console.ReadLine());
+    char[] firstWordArray = firstWord.ToArray();
+
+    Console.WriteLine("Input second word: ");
+    string secondWord = Convert.ToString(Console.ReadLine());
+    char[] secondWordArray = secondWord.ToArray();
+
+    List<string> mutual = new List<string>();
+    
+
+    for (int i = 0; i < firstWordArray.Length; i++)
+    {
+        for(int j = 0; j < secondWordArray.Length; j++)
+        {
+            if (firstWordArray[i] == secondWordArray[j])
+            {
+
+                mutual.Add(firstWordArray[i]);
+            }
+        }
+    }
+    var backToString = String.Concat(mutual);
+    Console.WriteLine(backToString);
+}
